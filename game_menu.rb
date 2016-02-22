779a52b54efcf624ee3c99a2c4df405088259c84
Justin Ewalt <justinewalt@gmail.com>
2016-02-22 14:33:39 -0700

    Initial commit for slots, same game_menu file

--- game_menu.rb
+++ game_menu.rb
@@ -1,22 +1,26 @@
-
 require_relative 'hi_lo'
-#require_relative 'Slots'
+require_relative 'Slots'
 
-def main_menu
-  puts "Welcome inside the Casino! What game would you like to play first?"
-  puts "Please make your game selection via by typing 1 or 2"
-  puts "1: Hi-Low"
-  puts "2: Slots"
-  @game_menu_input = gets.strip.to_i
-  menu_logic
-end
-def menu_logic
-    case @game_menu_input
-        when 1
-            HiLo.new(player)
-        when 2
-            ##
-        else
-            puts "Please make a selection between 1 -2!"
-    end
+class Game_Menu
+
+	def main_menu
+		puts "Welcome inside the Casino! What game would you like to play first?:"
+		puts "Please make your game selection via by typing 1 or 2"
+		puts "1: Hi-Low"
+		puts "2: Slots"
+		@game_menu_input = gets.strip.to_i
+		menu_logic
+	end
+
+	def menu_logic
+		case @game_menu_input
+			when 1
+				Highlo.new
+			when 2
+				Slots.new
+			else
+				puts "Please make a selection between 1 - 2!"
+		end
+		main_menu
+	end
 end
---
+++ slots.rb
@@ -0,0 +1,49 @@
+require_relative 'casino'
+
+class Slots
+	attr_accessor :
+
+	def initialize
+		puts "Welcome to Slots! Please enter your wager/bet:"
+		@bet = gets.strip.to_i
+		puts "Now type 'p' or 'pull' to begin!"
+		@user_input = gets.strip.downcase
+			if @user_input == 'p' || 'pull'
+				slots_start
+			else
+				puts "Please type 'p' or 'pull' to pull my finger and begin:"
+				initialize
+			end
+		puts "Here are you numbers #{@slots_numbers}"
+	end
+end
+
+def slots_start
+	
+
+	@slots_numbers = [rand(1..3), rand(1..3), rand(1..3)]
+	puts @slots_numbers
+
+
+
+end
+
+
+
+def slots_logic
+	case @slots_numbers
+		when 1, 1, 1
+			puts "Congratulations, you won!"
+
+		when 2, 2, 2
+			puts "Congratulations, you won!"
+
+
+		when 3, 3, 3
+			puts "Congratulations, you won!"
+
+
+	end
+end
+
+initialize
