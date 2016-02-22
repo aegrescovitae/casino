aa998ff2c17c32ca857bf9e53f5c69b0d54b2660
Garrett Stot <garrettstott@gmail.com>
2016-02-22 13:25:17 -0700

    "Created deck.rb, card.rb, and hi_lo.rb

---
+++ hi_lo.rb
@@ -0,0 +1,84 @@
+# require_relative 'player'
+
+class HiLo
+  attr_accessor :num_1, :num_2
+  def initialize
+    @num_1 = num_1
+    @num_2 = num_2
+  end
+end
+
+def menu
+  puts "-- WELCOME TO HIGH-LOW --"
+  random_numbers
+  input_bet
+end
+
+def random_numbers
+  random_numbers = rand(1..101)
+  @num_1 = random_numbers
+  random_numbers = rand(1..101)
+  @num_2 = random_numbers
+end
+
+def input_bet
+  puts "Please enter your bet amount!"
+  print "> "
+  @bet = gets.strip.to_f
+  hi_lo_game
+end
+
+def hi_lo_game
+  puts "First Number: #{@num_1}"
+  puts "Do you think the next number"
+  puts "will be higher, or lower?"
+  print "> "
+  input_hilo
+end
+
+def input_hilo
+  @input = gets.strip
+  if @input == 'higher'
+    puts "#{@num_2}"
+    high
+  elsif @input == 'lower'
+    puts "#{@num_2}"
+    low
+  else
+    puts 'Please enter high or low'
+    input_hilo
+  end
+end
+
+def high
+  if @num_1 > @num_2
+    puts 'false'
+  else
+    puts 'correct'
+  end
+  menu
+end
+
+def low
+  if @num_1 < @num_2
+    puts "false"
+  else
+    puts "correct"
+  end
+  menu
+end
+
+new_game = HiLo.new
+menu
+
+
+
+
+# HighLo
+# place a bet
+# player picks a number between 1 - 100
+# validate the input is between 1 - 100
+# have computer pick a random between 1 - 100
+# whos higher
+# calculate wins / loss
+# rebalance wallet
